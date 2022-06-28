a9ec6fcf95544ea2fd901d08931c08dcc4c9c96d
Haervwe <juan-jose-007@hotmail.com>
2022-06-28 20:09:54 -0300

    add form to add new books and styling to it

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 const booksArray = [];
+const inputForm = document.getElementById("inputForm");
+const newBookBtn = document.getElementById("new")
+const cancel = document.getElementById("cancel");
+const board = document.querySelector(".library");
 
-function newBook(title,author,pages,read){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
     this.read = read;
 }
+
+Book.prototype.addBook = function() {
+    const newBook = document.createElement("div");
+    newBook.className = "book";
+    const tit  = document.createElement("p");
+    tit.innerText = `Title: ${this.title}`;
+    const aut = document.createElement("p");
+    aut.innerText =`Author: ${this.author}`;
+    const pag = document.createElement("p");
+    pag.innerText = `N° of pages: ${this.pages}`;
+    const re = document.createElement("div");
+    if (this.read == true) {
+        re.className = "readed";
+    } else {
+        re.className = "notReaded";
+    }
+    newBook.appendChild(tit);
+    newBook.appendChild(aut);
+    newBook.appendChild(pag);
+    newBook.appendChild(re);
+    board.appendChild(newBook);
+}
+
+function showForm () {
+    inputForm.style.display = "grid";    
+}
+
+function hideForm () {
+    inputForm.style.display = "none";    
+}
+
+newBookBtn.addEventListener("click",showForm)
+cancel.addEventListener("click",hideForm)
 
 
 
